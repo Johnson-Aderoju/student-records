7ab80a89152c1dc12c8c125ef9f4ae5063f16d75
Johnson-Aderoju <Aderenle4life@gmail.com>
2022-05-05 21:04:40 +0100

    first commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository
+    ) {
+        return args -> {
+            Student mariam = new Student(1L,
+                    "Mariam",
+                    "mariam@gmail,com",
+                    LocalDate.of(2000, JANUARY, 5)
+            );
+            Student deroju= new Student(1L,
+                    "Deroju",
+                    "alex@gmail,com",
+                    LocalDate.of(2003, JANUARY, 8)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, deroju)
+            );
+
+
+        };
+    }
+}
